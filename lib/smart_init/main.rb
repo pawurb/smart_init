df0b36d6b727e76e9c5dce4bb81e4640cf6f8a2b
Morgan VanYperen <morgvanny@gmail.com>
2020-04-11 03:30:19 -0700

    add public_accessors option

--- lib/smart_init/main.rb
+++ lib/smart_init/main.rb
@@ -11,41 +11,33 @@ module SmartInit
     end
   end
 
-  def initialize_with_hash *attributes
-    public_readers_filter = -> (el) {
-      el.is_a?(Hash) && el.keys == [:public_readers]
-    }
-    attributes = attributes.map do |el|
-      if el.is_a?(Hash)
-        el.map { |k, v| { k => v } }
-      else
-        el
-      end
-    end.flatten
+  def initialize_with_hash(*required_attrs, **hash)
 
-    public_readers = attributes.select(&public_readers_filter)
-    attributes.delete_if(&public_readers_filter)
-    required_attrs = attributes.select { |el| el.is_a?(Symbol) }
-
-    default_value_attrs = attributes.select { |el| el.is_a?(Hash) }.reduce(Hash.new, :merge) || {}
+    public_readers = hash.delete(:public_readers) || []
+    public_accessors = hash.delete(:public_accessors) || []
+    if  public_readers == true || public_accessors == true
+      public_readers = required_attrs
+      public_accessors = required_attrs if public_accessors == true
+    else
+      public_readers += public_accessors
+    end
 
     define_method :initialize do |*parameters|
-      parameters = [{}] if parameters == []
-      unless parameters.first.is_a?(Hash)
+      parameters == [] ? parameters = {} : parameters = parameters.first
+      unless parameters.is_a?(Hash)
         raise ArgumentError, "invalid input, expected hash of attributes"
       end
 
       required_attrs.each do |required_attr|
-        unless parameters.first.has_key?(required_attr)
+        unless parameters.has_key?(required_attr)
           raise ArgumentError, "missing required attribute #{required_attr}"
         end
       end
-
-      (required_attrs + default_value_attrs.keys).each do |attribute|
-        value = if parameters.first.has_key?(attribute)
-          parameters.first.fetch(attribute)
+      (required_attrs + hash.keys).each do |attribute|
+        value = if parameters.has_key?(attribute)
+          parameters.fetch(attribute)
         else
-          default_value_attrs[attribute]
+          hash[attribute]
         end
 
         instance_variable_set("@#{attribute}", value)
@@ -53,18 +45,12 @@ module SmartInit
     end
 
     instance_eval do
-      all_readers = (required_attrs + default_value_attrs.keys).compact
+      all_readers = (required_attrs + hash.keys)
       attr_reader(*all_readers)
-
-      if public_readers.count == 0
-        all_readers.each do |method_name|
-          private method_name
-        end
-      elsif public_readers.first.fetch(:public_readers).is_a?(Array)
-        (all_readers - public_readers.first.fetch(:public_readers)).each do |method_name|
-          private method_name
-        end
+      (all_readers - public_readers).each do |reader|
+        private reader
       end
+      attr_writer(*public_accessors)
     end
   end
 
